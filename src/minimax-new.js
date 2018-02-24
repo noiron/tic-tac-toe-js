164d326e6f08c6f40cb7130fb35450ca32cff4ac
wu kai <wukai2048@gmail.com>
2018-02-24 10:47:39 +0800

    add new minimax file

---
+++ src/minimax-new.js
@@ -0,0 +1,53 @@
+import { getAiToken, X, O } from './main';
+
+class GameState {
+    constructor(data, type, depth) {
+        this.data = data;
+        this.type = type; // 区分此节点的种类是 max 或 min
+        this.depth = depth;
+    }
+
+    score() {
+        // 到达了最大深度后，此时的 data 是数组最内层的数字
+        if (this.depth >= 4) {
+            return this.data;
+        }
+
+        // 对于 max 节点，返回的是子节点中的最大值
+        if (this.type === 'max') {
+            let maxScore = -1000;
+            let maxIndex = 0;
+
+            for (let i = 0; i < this.data.length; i++) {
+                const d = this.data[i];
+                const childNode = new GameState(d, changeType(this.type), this.depth + 1);
+                const childScore = childNode.score();
+
+                if (childScore > maxScore) {
+                    maxScore = childScore;
+                    maxIndex = i;
+                }
+            }
+
+            return maxScore;
+        }
+
+        // 对于 min 节点，返回的是子节点中的最小值
+        else if (this.type === 'min') {
+            let minScore = 1000;
+            let minIndex = 0;
+        
+            for (let i = 0; i < this.data.length; i++) {
+                const d = this.data[i];
+                const childNode = new Node(d, changeType(this.type), this.depth + 1);
+                const childScore = childNode.score();
+
+                if (childScore < minScore) {
+                    minScore = childScore;
+                    minIndex = i;
+                }
+            }
+            return minScore;
+        }
+    }
+}
